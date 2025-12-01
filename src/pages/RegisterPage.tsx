55bce6678f479a819fca0a870dfcce514d01acc5
Jakub Abram <j.abram1@wp.pl>
2025-12-01 21:32:30 +0100

    Finished register page

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,5 +1,10 @@
+import RegisterForm from "../auth/components/RegisterForm"
+
 export default function RegisterPage (){
-    return <h1>
-        Register Page
-    </h1>
+    return <div className="w-full max-w-lg mx-auto py-12">
+        <h1 className="w-full text-center text-black font-bold">
+            Register now!
+        </h1>
+        <RegisterForm />
+    </div>
 }
