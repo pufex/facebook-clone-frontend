e60449481da93dfcd3f21a92a4a7ff9d43e96316
Jakub Abram <j.abram1@wp.pl>
2025-12-01 18:58:01 +0100

    Created login function and fetching profile picture and background picture

--- src/auth/AuthProvider.tsx
+++ src/auth/AuthProvider.tsx
@@ -1,5 +1,5 @@
-import { createContext, useState, type JSX, useCallback } from "react";
-import type { RegisterObject, User } from "../types";
+import { createContext, useState, type JSX, useCallback, useRef, useEffect } from "react";
+import type { ImageChunk, ImageDeclaration, LoginObject, RegisterObject, ResponseUser, User } from "../types";
 import { axiosPublic } from "../api/api";
 
 type AuthObject = {
@@ -7,9 +7,17 @@ type AuthObject = {
     accessToken: string
 }
 
+type AuthState = AuthObject | null
+
+type ResponseAuthObject = {
+    user: ResponseUser,
+    accessToken: string,
+}
+
 type AuthContextType = {
-    auth: AuthObject | null,
-    register: (data: RegisterObject) => Promise<void>
+    auth: AuthState,
+    register: (data: RegisterObject) => Promise<void>,
+    login: (data: LoginObject) => Promise<void>
 } | null
 
 type AuthProviderProps = {
@@ -20,7 +28,9 @@ export const AuthContext = createContext<AuthContextType>(null)
 
 export default function AuthProvider({ children }: AuthProviderProps) {
 
-    const [auth, setAuth] = useState(null)
+    const [auth, setAuth] = useState<AuthState>(null)
+    const profile_pic_id = useRef<string>(null)
+    const background_pic_id = useRef<string>(null)
 
     const register = useCallback(async (data: RegisterObject) => {
         try {
@@ -34,7 +44,194 @@ export default function AuthProvider({ children }: AuthProviderProps) {
         }
     }, [])
 
-    return <AuthContext.Provider value={{ auth, register }}>
+    const login = useCallback(async (data: LoginObject) => {
+        try {
+            const response = await axiosPublic.post(
+                "/auth/register",
+                data,
+                {
+                    withCredentials: true,
+                    headers: { "Content-Type": "application/json" }
+                }
+            )
+            const authResponseObject = response.data as ResponseAuthObject
+            profile_pic_id.current = authResponseObject.user.profile_picture_id
+            background_pic_id.current = authResponseObject.user.background_picture_id
+            setAuth({
+                ...authResponseObject,
+                user: {
+                    ...authResponseObject.user,
+                    profile_picture: authResponseObject.user.profile_picture_id
+                        ? {
+                            id: authResponseObject.user.profile_picture_id,
+                            loading: true,
+                            error: "",
+                            data: "",
+                        }
+                        : null,
+                    background_picture: authResponseObject.user.background_picture_id
+                        ? {
+                            id: authResponseObject.user.background_picture_id,
+                            loading: true,
+                            error: "",
+                            data: "",
+                        }
+                        : null
+                }
+            })
+        } catch (err) {
+            throw err
+        }
+    }, [])
+
+    useEffect(() => {
+        const fetchProfilePicture = async () => {
+            if (profile_pic_id.current && auth && auth.user.profile_picture && auth.user.profile_picture.id !== profile_pic_id.current) {
+                try {
+                    const declaration_id = auth.user.profile_picture.id
+                    profile_pic_id.current = declaration_id
+                    const declarationResponse = await axiosPublic.get(`/images/declaration/${declaration_id}`)
+                    const declaration = declarationResponse.data as ImageDeclaration
+                    setAuth(prev => !prev
+                        ? prev
+                        : {
+                            ...prev,
+                            user: {
+                                ...prev.user,
+                                profile_picture: {
+                                    id: declaration_id,
+                                    loading: true,
+                                    error: "",
+                                    data: ""
+                                }
+                            }
+                        }
+                    )
+                    const chunksNumbers = []
+                    for (let i = 0; i < declaration.chunksAmount; i++) {
+                        chunksNumbers.push(i + 1)
+                    }
+                    const listedChunks: Pick<ImageChunk, "chunkNumber" | "data">[] = await Promise.all(chunksNumbers.map(async (chunkNumber) => {
+                        const chunkResponse = await axiosPublic.post(`/images/chunk/${declaration_id}`, { chunkNumber })
+                        const chunk = chunkResponse.data as ImageChunk
+                        return { chunkNumber, data: chunk.data }
+                    }))
+                    setAuth(prev => !prev
+                        ? prev
+                        : {
+                            ...prev,
+                            user: {
+                                ...prev.user,
+                                profile_picture: prev.user.profile_picture
+                                    ? {
+                                        ...prev.user.profile_picture,
+                                        loading: false,
+                                        data: listedChunks
+                                            .sort((a, b) => a.chunkNumber - b.chunkNumber)
+                                            .map(chunk => chunk.data)
+                                            .join("")
+                                    }
+                                    : prev.user.profile_picture,
+                            }
+                        }
+                    )
+                } catch (err) {
+                    console.log(err)
+                    setAuth(prev => !prev
+                        ? prev
+                        : {
+                            ...prev,
+                            user: {
+                                ...prev.user,
+                                profile_picture: {
+                                    id: "",
+                                    loading: false,
+                                    error: "Failed to fetch",
+                                    data: ""
+                                }
+                            }
+                        }
+                    )
+                }
+            }
+        }
+
+        const fetchBackgroundPicture = async () => {
+            if (background_pic_id.current && auth && auth.user.background_picture && auth.user.background_picture.id !== background_pic_id.current) {
+                try {
+                    const declaration_id = auth.user.background_picture.id
+                    profile_pic_id.current = declaration_id
+                    const declarationResponse = await axiosPublic.get(`/images/declaration/${declaration_id}`)
+                    const declaration = declarationResponse.data as ImageDeclaration
+                    setAuth(prev => !prev
+                        ? prev
+                        : {
+                            ...prev,
+                            user: {
+                                ...prev.user,
+                                background_picture: {
+                                    id: declaration_id,
+                                    loading: true,
+                                    error: "",
+                                    data: ""
+                                }
+                            }
+                        }
+                    )
+                    const chunksNumbers = []
+                    for (let i = 0; i < declaration.chunksAmount; i++) {
+                        chunksNumbers.push(i + 1)
+                    }
+                    const listedChunks: Pick<ImageChunk, "chunkNumber" | "data">[] = await Promise.all(chunksNumbers.map(async (chunkNumber) => {
+                        const chunkResponse = await axiosPublic.post(`/images/chunk/${declaration_id}`, { chunkNumber })
+                        const chunk = chunkResponse.data as ImageChunk
+                        return { chunkNumber, data: chunk.data }
+                    }))
+                    setAuth(prev => !prev
+                        ? prev
+                        : {
+                            ...prev,
+                            user: {
+                                ...prev.user,
+                                background_picture: prev.user.background_picture
+                                    ? {
+                                        ...prev.user.background_picture,
+                                        loading: false,
+                                        data: listedChunks
+                                            .sort((a, b) => a.chunkNumber - b.chunkNumber)
+                                            .map(chunk => chunk.data)
+                                            .join("")
+                                    }
+                                    : prev.user.background_picture,
+                            }
+                        }
+                    )
+                } catch (err) {
+                    console.log(err)
+                    setAuth(prev => !prev
+                        ? prev
+                        : {
+                            ...prev,
+                            user: {
+                                ...prev.user,
+                                background_picture: {
+                                    id: "",
+                                    loading: false,
+                                    error: "Failed to fetch",
+                                    data: ""
+                                }
+                            }
+                        }
+                    )
+                }
+            }
+        }
+
+        fetchProfilePicture();
+        fetchBackgroundPicture();
+    }, [auth])
+
+    return <AuthContext.Provider value={{ auth, register, login }}>
         {children}
     </AuthContext.Provider>
 }
