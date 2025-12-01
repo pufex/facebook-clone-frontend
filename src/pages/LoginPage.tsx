766ccac775905f4c9af680e3ccf3f513905d151d
Jakub Abram <j.abram1@wp.pl>
2025-12-01 21:33:16 +0100

    Finished login page

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,5 +1,10 @@
-export default function LoginPage(){
-    return <h1>
-        Login Page
-    </h1>
+import LoginForm from "../auth/components/LoginForm"
+
+export default function LoginPage (){
+    return <div className="w-full max-w-lg mx-auto py-12">
+        <h1 className="w-full text-center text-black font-bold">
+            Log in
+        </h1>
+        <LoginForm />
+    </div>
 }
